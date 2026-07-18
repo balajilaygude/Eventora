859ea914ee4bdc1ee141e357f0677cc534347b01
Balaji Laygude <balajiwork05@gmail.com>
2026-07-18 19:39:28 +0530

    Email log

--- Backend/Utils/email.js
+++ Backend/Utils/email.js
@@ -11,33 +11,36 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendOtpEmail(email, otp, type) {
+  console.log("Email start")
   try {
+    console.log("Email in")
     const title =
-      type === "account_verification"
-        ? "Verify your Eventora Account :"
-        : "Event Booking Verification";
+    type === "account_verification"
+    ? "Verify your Eventora Account :"
+    : "Event Booking Verification";
     const msg =
-      type === "account_verification"
-        ? "Please use Following OTP to verify your Eventora Account"
-        : "Please use Following OTP to confirm your Event Booking";
-
+    type === "account_verification"
+    ? "Please use Following OTP to verify your Eventora Account"
+    : "Please use Following OTP to confirm your Event Booking";
+    
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
       subject: title,
       html: `
-        <div style="width:250px;padding:20px;border:1px solid #ddd;border-radius:8px;text-align:center;font-family:Arial,sans-serif;">
-          <h2 style="margin:0 0 10px;">${title}</h2>
-          <p style="margin:8px 0;">
-            ${msg}
-          </p>
-          <p style="margin:8px 0;font-size:24px;font-weight:bold;letter-spacing:4px;">
-            ${otp}
-          </p>
-        </div>
+      <div style="width:250px;padding:20px;border:1px solid #ddd;border-radius:8px;text-align:center;font-family:Arial,sans-serif;">
+      <h2 style="margin:0 0 10px;">${title}</h2>
+      <p style="margin:8px 0;">
+      ${msg}
+      </p>
+      <p style="margin:8px 0;font-size:24px;font-weight:bold;letter-spacing:4px;">
+      ${otp}
+      </p>
+      </div>
       `,
     };
     await transporter.sendMail(mailOptions);
+    console.log("Email in")
     console.log(`Email.js Otp for ${email} for ${otp}`);
   } catch (error) {
     console.error(`Error sending otp email ${email} for ${otp}`, error);
