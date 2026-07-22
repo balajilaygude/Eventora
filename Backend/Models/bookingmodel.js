899817ff7a07beaa2345a0fd822e0901e5c3abc8
Balaji Laygude <balajiwork05@gmail.com>
2026-07-22 21:14:12 +0530

    Booking modal change

--- Backend/Models/bookingmodel.js
+++ Backend/Models/bookingmodel.js
@@ -1,33 +1,35 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const bookingShcema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
+const bookingShcema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    eventId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Event",
-        required:true,        
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
     },
-    status:{
-        type:String,
-        enum:["pending","confimed","cancelled"],
-        default:"pending"
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
     },
-    paymentStatus:{
-        type:String,
-        enum:["non_paid","paid"],
-        default:"non_paid",
+    paymentStatus: {
+      type: String,
+      enum: ["non_paid", "paid"],
+      default: "non_paid",
     },
-    amount:{
-        type:Number,
-        required:true
-    }
-},{timestamps:true})
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-const bookingModel=mongoose.model("Booking",bookingShcema)
+const bookingModel = mongoose.model("Booking", bookingShcema);
 
-module.exports=bookingModel
-
+module.exports = bookingModel;
